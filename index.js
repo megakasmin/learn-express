318ec977a768dd544dbe183b05d80ac6fb5e91d3
megakasmin <meghakasmin99@gmail.com>
2019-11-26 17:20:26 +0700

    change models

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 
